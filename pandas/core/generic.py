d7a04ce8ca51c5b7a873dc006a91e3744ceb98ee
cldy <charlotte.dyckmanns@kabeldeutschland.de>
2016-02-08 13:14:27 +0100

    BUG: subclassing

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -1698,7 +1698,6 @@ class NDFrame(PandasObject):
                 new_index = self.index[loc]
 
         if np.isscalar(loc):
-            from pandas import Series
             new_values = self._data.fast_xs(loc)
 
             # may need to box a datelike-scalar
@@ -1709,9 +1708,10 @@ class NDFrame(PandasObject):
             if not is_list_like(new_values) or self.ndim == 1:
                 return _maybe_box_datetimelike(new_values)
 
-            result = Series(new_values, index=self.columns,
-                            name=self.index[loc], copy=copy,
-                            dtype=new_values.dtype)
+            result = self._constructor_sliced(
+                new_values, index=self.columns,
+                name=self.index[loc], copy=copy,
+                dtype=new_values.dtype)
 
         else:
             result = self.iloc[loc]
@@ -4814,7 +4814,8 @@ class NDFrame(PandasObject):
                           [pretty_name(x) for x in percentiles] + ['max'])
             d = ([series.count(), series.mean(), series.std(), series.min()] +
                  [series.quantile(x) for x in percentiles] + [series.max()])
-            return pd.Series(d, index=stat_index, name=series.name)
+            return self._constructor_sliced(
+                d, index=stat_index, name=series.name)
 
         def describe_categorical_1d(data):
             names = ['count', 'unique']
@@ -4835,7 +4836,8 @@ class NDFrame(PandasObject):
                                lib.Timestamp(asint.min()),
                                lib.Timestamp(asint.max())]
 
-            return pd.Series(result, index=names, name=data.name)
+            return self._constructor_sliced(
+                result, index=names, name=data.name)
 
         def describe_1d(data, percentiles):
             if com.is_numeric_dtype(data):
@@ -4871,7 +4873,7 @@ class NDFrame(PandasObject):
                     names.append(name)
         d = pd.concat(ldesc, join_axes=pd.Index([names]), axis=1)
         d.columns.names = data.columns.names
-        return d
+        return self._constructor(d)
 
     def _check_percentile(self, q):
         """Validate percentiles (used by describe and quantile)."""
